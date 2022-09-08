0244946f5068b12089dfb1b4b4eaa7672cd5a3f5
Aditya <aditya.singh@itspe.co.in>
2022-09-08 15:19:05 +0530

    single file input

--- config_validator.py
+++ config_validator.py
@@ -1,28 +1,11 @@
 from datetime import datetime
 import json
 
-time_table_open = open('pre_config_files/TimeTable.json')
-site_data_open = open('pre_config_files/SiteData.json')
-period_phase_limit_open = open('pre_config_files/PeriodPhaseLimits.json')
-phase_conflict_open = open('pre_config_files/PhaseConflict.json')
-inter_phase_duration_open = open('pre_config_files/InterPhaseDuration.json')
-smartmicro_open = open('pre_config_files/SmartMicro.json')
-phase_detector_open = open('pre_config_files/ApproachPhaseDetectorMap.json')
-approach_detector_open = open('pre_config_files/ApproachPhaseApproach.json')
-
-
-time_table=json.load(time_table_open)
-site_data=json.load(site_data_open)
-period_phase_limit = json.load(period_phase_limit_open)
-phase_conflict = json.load(phase_conflict_open)
-inter_phase_duration = json.load(inter_phase_duration_open)
-smartmicro = json.load(smartmicro_open)
-phase_detector = json.load(phase_detector_open)
-approach_detector = json.load(approach_detector_open)
-
+junction_data_open = open('data.json')
+junction_data = json.load(junction_data_open)
 
 #Check 0 period
-def check_0_period(period_phase_limit) :                            #Check : 0 period not defined in the TimeTable only in PeriodPhaseLimit. 
+def check_0_period(period_phase_limit) :                             
     '''
     Check for 0 period in the time_table
     '''
@@ -58,7 +41,7 @@ def check_offline_mode(time_table) :
                     j+=1
     if error_msg == {} :
         return None
-    return error_msg                                                                #Check weather to return empty msg or None in case no error
+    return error_msg                                                               
 
 
 #Overlapping
@@ -99,8 +82,8 @@ def manager(time_table) :
     j=0
     for key in time_table :
         day_schedule = create_interval_array(time_table,key)
-        n=len(day_schedule)-1
-        overlap_bool = check_overlap(day_schedule[1:],n)            #Not taking first slot of each day as 18:38---
+        n=len(day_schedule)
+        overlap_bool = check_overlap(day_schedule,n)            #Not taking first slot of each day as 18:38---
         if overlap_bool :
             error_add ={
                 'Error' : 'Overlapping time slot in time table',
@@ -229,7 +212,7 @@ def check_radar_ip(smartmicro) :
     error_msg = {}
     j=0
     for key in smartmicro['details'] :
-        if smartmicro['details'][key]['radar_ip'] == {} :
+        if smartmicro['details'][key]['radar_ip'] == "" :
             add_msg = {
                 'Error' : 'IP address missing for radar detector',
                 'Radar ID' : smartmicro['details'][key]['radar_id']
@@ -242,16 +225,16 @@ def check_radar_ip(smartmicro) :
 
 
 #Approach Phase Detector Map
-def check_approach_phase_detector_map(phase_detector) :
+def check_approach_phase_detector_map(approach_detector) :
     '''
     Phase to approach mapping cannot be empty.
     '''
     error_msg={}
     phase_list=[]
-    for key in phase_detector :
-        for key_1 in phase_detector[key] :
-            if phase_detector[key][key_1] != '':
-                phase_list.append(phase_detector[key][key_1])
+    for key in approach_detector :
+        for key_1 in approach_detector[key] :
+            if approach_detector[key][key_1] != '':
+                phase_list.append(approach_detector[key][key_1])
     if phase_list == [] :
         error_msg = {
             'Error' : 'Approach Phase Detector Map is empty'
@@ -260,10 +243,20 @@ def check_approach_phase_detector_map(phase_detector) :
         return None
     return error_msg
 
-def validation_manager() :
+def validation_manager(junction_data) :
     '''
     Return the final json file
     '''
+    time_table = json.loads(junction_data['TimeTable'])
+    site_data  = json.loads(junction_data['SiteData'])
+    period_phase_limit = json.loads(junction_data['PeriodPhaseLimits'])
+    phase_conflict=json.loads(junction_data['PhaseConflict'])
+    inter_phase_duration = json.loads(junction_data['InterPhaseDuration'])
+    smartmicro = json.loads(junction_data['DetectorConfig']['SMC'])
+    phase_detector = json.loads(junction_data['PhaseDetectorMap'])
+    approach_detector = json.loads(junction_data['ApproachPhaseApproach'])
+    
+    
     zero_period = check_0_period(period_phase_limit)
     offline_mode = check_offline_mode(time_table)
     overlap = manager(time_table)
@@ -272,7 +265,7 @@ def validation_manager() :
     detector_scn = check_detector_scn(smartmicro,phase_detector)
     phase_detector_check = phase_detector_consistency(phase_detector,approach_detector)
     radar_ip = check_radar_ip(smartmicro)
-    approach_phase_detector_map = check_approach_phase_detector_map(phase_detector)
+    approach_phase_detector_map = check_approach_phase_detector_map(approach_detector)
 
     final_msg = json.dumps({
         'Zero period' : zero_period,
@@ -288,4 +281,4 @@ def validation_manager() :
 
     return final_msg
 
-#print(validation_manager())
+# print(validation_manager(junction_data))
